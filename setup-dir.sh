4a3cc430a237b6549d1091a993899c465d95a076
jordan2lee <jordanl.7890@gmail.com>
2019-12-12 00:37:57 +0000

    update Dockerfile and new script to set up dirs

---
+++ setup-dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Create dirs for repo
+mkdir scripts
+mkdir scripts/01_process-bulkrna
+mkdir scripts/02_process-scrna
+mkdir scripts/03_compare-bulk_scrna
+
+mkdir data
+mkdir data/01_process-bulkrna
+mkdir data/02_process-scrna
+mkdir data/03_compare-bulk_scrna
+
+mkdir raw-data
+
+mkdir sandbox
+
