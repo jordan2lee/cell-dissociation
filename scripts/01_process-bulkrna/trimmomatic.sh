8724c8086365205de49eb54ac670336c65e87a61
jordan2lee <jordanl.7890@gmail.com>
2019-12-27 13:00:06 -0800

    add bulkrna trimmomatic.sh

---
+++ scripts/01_process-bulkrna/trimmomatic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+############
+# Filter/trim raw reads
+############
+echo '####################'
+echo 'Filter/trim raw reads - Trimmomatic version'
+java -jar ../../apps/Trimmomatic-0.39/trimmomatic-0.39.jar -version
+echo '####################'
