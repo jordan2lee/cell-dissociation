6c3d8d44cf5d63fa1ef67a4f7e9590af3c86f548
jordan2lee <jordanl.7890@gmail.com>
2019-12-27 13:32:01 -0800

    mod trimmomatic.sh so that runs, still need to opt flags

--- scripts/01_process-bulkrna/trimmomatic.sh
+++ scripts/01_process-bulkrna/trimmomatic.sh
@@ -2,8 +2,17 @@
 
 ############
 # Filter/trim raw reads
+#   Man: http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/TrimmomaticManual_V0.32.pdf
 ############
+inpath=${1}
+outpath=${2}
+
 echo '####################'
 echo 'Filter/trim raw reads - Trimmomatic version'
-java -jar ../../apps/Trimmomatic-0.39/trimmomatic-0.39.jar -version
+# java -jar ../../apps/Trimmomatic-0.39/trimmomatic-0.39.jar -version
 echo '####################'
+
+java -jar ../../apps/Trimmomatic-0.39/trimmomatic-0.39.jar \
+    PE ${inpath}/example_r1-da.fq.gz ${inpath}/example_r2-da.fq.gz \
+    -baseout ${outpath}/example-trim.fq.gz \
+    LEADING:3 TRAILING:3 MINLEN:36
