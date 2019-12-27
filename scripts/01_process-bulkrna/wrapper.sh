4b1c6279bd10fc784ecfaac0c184ec03eca48ce6
jordan2lee <jordanl.7890@gmail.com>
2019-12-27 13:31:02 -0800

    update wrapper to input .gz files for fastqc

--- scripts/01_process-bulkrna/wrapper.sh
+++ scripts/01_process-bulkrna/wrapper.sh
@@ -4,6 +4,7 @@
 # Quality filtering and trimming
 ###############
 # Create raw read quality report
-bash fastqc.sh example_r1.fq example_r2.fq
-# Filter and/or trim as per above quality report
-bash trimmomatic.sh
+bash fastqc.sh example_r1.fq.gz example_r2.fq.gz
+
+# # Filter and/or trim as per above quality report
+# bash trimmomatic.sh ../../data/01_process-bulkrna/01_demultiplex_and_adpttrim ../../data/01_process-bulkrna/03_readtrim_and_filter
