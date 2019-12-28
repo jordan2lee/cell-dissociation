fec6e88a43fe1caa038bf37398f64f5f1569522d
jordan2lee <jordanl.7890@gmail.com>
2019-12-27 16:52:23 -0800

    update setup-dirs.sh to include star alignment

--- scripts/01_process-bulkrna/setup-dirs.sh
+++ scripts/01_process-bulkrna/setup-dirs.sh
@@ -7,3 +7,7 @@ mkdir ../../data/01_process-bulkrna/01_demultiplex_and_adpttrim
 mkdir ../../data/01_process-bulkrna/02_readqc
 # trim/filter raw read
 mkdir ../../data/01_process-bulkrna/03_readtrim_and_filter
+# star 
+mkdir ../../data/01_process-bulkrna/04_align
+mkdir ../../data/01_process-bulkrna/04_align/genome_indexes
+mkdir ../../data/01_process-bulkrna/04_align/mapped
