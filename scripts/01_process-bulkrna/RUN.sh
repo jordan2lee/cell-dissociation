7eca9e20f33aa1755aab6ad2bba206730dcfa3a7
leejor <leejor@metal-1-0.local>
2020-02-20 09:39:23 -0800

    testing RUN.sh for exacloud for alignment and ct matrix steps

--- scripts/01_process-bulkrna/RUN.sh
+++ scripts/01_process-bulkrna/RUN.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --job-name=docker_pipeline
+#SBATCH --time=0-12:00:00
+#SBATCH --partition=exacloud
+#SBATCH --ntasks=12
+#SBATCH --cpus-per-task=1
+#SBATCH --output=../../logs/slurm.%N.%j.out
+#SBATCH --error=../../logs/slurm.%N.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=leejor@ohsu.edu
+
+
 dir=/home/groups/EllrottLab/cell-dissociation
 ses=round1
 read1=TESTDATA--UHR_Rep1_ERCC-Mix1_Build37-ErccTranscripts-chr22.read1.fastq.gz
@@ -10,6 +23,8 @@ basename=TESTDATA--UHR_Rep1_ERCC-Mix1_Build37-ErccTranscripts-chr22
 ################
 # Move to main dir
 cd ${dir}
+# clean up any remaining dirs
+rm -rf /mnt/scratch/5420
 # move files to scratch, computing cluster specific
 mkdir /mnt/scratch/5420
 mkdir /mnt/scratch/5420/${ses}
@@ -18,6 +33,11 @@ cp -r raw-data /mnt/scratch/5420/${ses} #move raw seq reads dir
 mkdir /mnt/scratch/5420/${ses}/output
 mkdir /mnt/scratch/5420/${ses}/output/02a_fastqc
 mkdir /mnt/scratch/5420/${ses}/output/03_trimmomatic
+mkdir /mnt/scratch/5420/${ses}/output/04_fastqc-trimmed
+mkdir /mnt/scratch/5420/${ses}/output/06_star/
+mkdir /mnt/scratch/5420/${ses}/output/06_star/genome_ref
+mkdir /mnt/scratch/5420/${ses}/output/06_star/mapped
+mkdir /mnt/scratch/5420/${ses}/output/07_ct_matrices
 
 #########################
 # QC raw reads - fastqc
@@ -30,9 +50,7 @@ sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
     /tmp/raw-data/${read1} \
     -f fastq \
     -o /tmp/output/02a_fastqc
-
 # Read 2
-sudo /opt/acc/sbin/exadocker pull biocontainers/fastqc:v0.11.8dfsg-2-deb_cv1
 sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
     biocontainers/fastqc:v0.11.8dfsg-2-deb_cv1 \
     fastqc \
@@ -40,80 +58,106 @@ sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
     -f fastq \
     -o /tmp/output/02a_fastqc
 
+####################################
+# Trim low qual reads - trimmomatic
+####################################
+sudo /opt/acc/sbin/exadocker pull quay.io/biocontainers/trimmomatic:0.39--1
+sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
+    quay.io/biocontainers/trimmomatic:0.39--1 \
+    trimmomatic PE \
+    -threads 11 -phred33 \
+    -trimlog /tmp/output/LOG_${basename}.txt\
+    /tmp/raw-data/${read1} \
+    /tmp/raw-data/${read2} \
+    /tmp/output/03_trimmomatic/${read1}_trimmed.fastq.gz \
+    /tmp/output/03_trimmomatic/${read1}_trimmedUNPAIRED.fastq.gz \
+    /tmp/output/03_trimmomatic/${read2}_trimmed.fastq.gz \
+    /tmp/output/03_trimmomatic/${read2}_trimmedUNPAIRED.fastq.gz \
+    LEADING:30 TRAILING:30\
+    SLIDINGWINDOW:4:30 \
+    MINLEN:90
 
-# # ####################################
-# # # Trim low qual reads - trimmomatic
-# # ####################################
-# sudo /opt/acc/sbin/exadocker pull quay.io/biocontainers/trimmomatic:0.39--1
-# sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
-#     quay.io/biocontainers/trimmomatic:0.39--1 \
-#     trimmomatic PE \
-#     -threads 11 -phred33 \
-#     -trimlog /tmp/output/LOG_${basename}.txt\
-#     /tmp/raw-data/${read1} \
-#     /tmp/raw-data/${read2} \
-#     /tmp/output/03_trimmomatic/${read1}_trimmed.fastq.gz \
-#     /tmp/output/03_trimmomatic/${read1}_trimmedUNPAIRED.fastq.gz \
-#     /tmp/output/03_trimmomatic/${read2}_trimmed.fastq.gz \
-#     /tmp/output/03_trimmomatic/${read2}_trimmedUNPAIRED.fastq.gz \
-#     LEADING:30 TRAILING:30\
-#     SLIDINGWINDOW:4:30 \
-#     MINLEN:90
-#
-# # want to automate this step
-# # QC raw reads - fastqc
-# cwl-runner --outdir ../../data/01_process-bulkrna/data_dump/04_fastqc-trimmed \
-#     workflows/fastqc_raw-workflow.cwl \
-#     tools/fastqc_trimmedinputs.yml
-#
-# # Build genome indexes for alignment
-# docker pull alexdobin/star:2.6.1d
-# zcat /home/groups/EllrottLab/cell-dissociation/data/01_process-bulkrna/data_dump/06_star/metadata/ref_fa/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz > /home/groups/EllrottLab/cell-dissociation/data/01_process-bulkrna/data_dump/06_star/metadata/ref_fa/TEMP-ref.fa
-# zcat /home/groups/EllrottLab/cell-dissociation/data/01_process-bulkrna/data_dump/06_star/metadata/gtf/Homo_sapiens.GRCh38.98.gtf.gz > /home/groups/EllrottLab/cell-dissociation/data/01_process-bulkrna/data_dump/06_star/metadata/gtf/TEMP-ref.gtf
-# docker run --rm -v /home/groups/EllrottLab/cell-dissociation:/tmp \
-#     alexdobin/star:2.6.1d \
-#     STAR \
-#     --runThreadN 11 \
-#     --runMode genomeGenerate \
-#     --genomeDir /tmp/data/01_process-bulkrna/data_dump/06_star/genome_indexes \
-#     --genomeFastaFiles /tmp/data/01_process-bulkrna/data_dump/06_star/metadata/ref_fa/TEMP-ref.fa \
-#     --sjdbGTFfile /tmp/data/01_process-bulkrna/data_dump/06_star/metadata/gtf/TEMP-ref.gtf \
-#     --sjdbOverhang 100 \
-#     --genomeSAsparseD 10
-# rm ../../data/01_process-bulkrna/data_dump/06_star/metadata/ref_fa/TEMP-ref.fa
-#
-# # Map Reads - output: unsorted BAM
-# docker pull alexdobin/star:2.6.1d
-# docker run --rm -v /home/groups/EllrottLab/cell-dissociation:/tmp \
-#     alexdobin/star:2.6.1d \
-#     STAR \
-#     --runThreadN 11 \
-#     --readFilesCommand zcat \
-#     --genomeDir /tmp/data/01_process-bulkrna/data_dump/06_star/genome_indexes \
-#     --readFilesIn /tmp/data/01_process-bulkrna/data_dump/03_trimmomatic/${read1}_trimmed.fastq.gz /tmp/data/01_process-bulkrna/data_dump/03_trimmomatic/${read2}_trimmed.fastq.gz \
-#     --outFileNamePrefix /tmp/data/01_process-bulkrna/data_dump/06_star/mapped/${basename}_mapped \
-#     --outSAMtype BAM Unsorted
-#
-# # BAM QC
-#
-#     # TBA
-#
-#
-# # Quantify reads: featureCounts
-# docker pull alexgilgal/featurecount:latest
-# docker run --rm -v /home/groups/EllrottLab/cell-dissociation:/tmp \
-#     alexgilgal/featurecount:latest \
-#     featureCounts \
-#     -T 10 -F GTF \
-#     -g gene_id -t exon \
-#     -R \
-#     -s 1 \
-#     -a /tmp/data/01_process-bulkrna/data_dump/06_star/metadata/gtf/TEMP-ref.gtf \
-#     -o /tmp/data/01_process-bulkrna/data_dump/07_ct_matrices/${basename}_CTmatrix.txt  \
-#     /tmp/data/01_process-bulkrna/data_dump/06_star/mapped/${basename}_mappedAligned.out.bam
-# rm ../../data/01_process-bulkrna/data_dump/06_star/metadata/gtf/TEMP-ref.gtf
+#########################
+# QC raw reads - fastqc
+#########################
+# Read 1
+sudo /opt/acc/sbin/exadocker pull biocontainers/fastqc:v0.11.8dfsg-2-deb_cv1
+sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
+    biocontainers/fastqc:v0.11.8dfsg-2-deb_cv1 \
+    fastqc \
+    /tmp/output/03_trimmomatic/${read1}_trimmed.fastq.gz \
+    -f fastq \
+    -o /tmp/output/04_fastqc-trimmed
+# Read 2
+sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
+    biocontainers/fastqc:v0.11.8dfsg-2-deb_cv1 \
+    fastqc \
+    /tmp/output/03_trimmomatic/${read2}_trimmed.fastq.gz \
+    -f fastq \
+    -o /tmp/output/04_fastqc-trimmed
 
+##########################
+# Build genome indexes for alignment
+#       genomeSAsparseD=1 (default)
+##########################
+sudo /opt/acc/sbin/exadocker pull alexdobin/star:2.6.1d
+zcat /mnt/scratch/5420/${ses}/raw-data/metadata-STAR/ref_fa/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz > /mnt/scratch/5420/${ses}/raw-data/metadata-STAR/ref_fa/TEMP-ref.fa
+zcat /mnt/scratch/5420/${ses}/raw-data/metadata-STAR/gtf/Homo_sapiens.GRCh38.98.gtf.gz > /mnt/scratch/5420/${ses}/raw-data/metadata-STAR/gtf/TEMP-ref.gtf
+echo '### finished unzipping files ###'
+sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
+    alexdobin/star:2.6.1d \
+    STAR \
+    --runThreadN 11 \
+    --runMode genomeGenerate \
+    --genomeDir /tmp/output/06_star/genome_ref/ \
+    --genomeFastaFiles /tmp/raw-data/metadata-STAR/ref_fa/TEMP-ref.fa \
+    --sjdbGTFfile /tmp/raw-data/metadata-STAR/gtf/TEMP-ref.gtf \
+    --sjdbOverhang 100 \
+    --outFileNamePrefix /tmp/output/06_star/genome_ref/Log \
+    --genomeSAsparseD 1
 
+####################################
+# Map Reads - output: unsorted BAM
+####################################
+##########
+# testing
+# cp -r /home/groups/EllrottLab/cell-dissociation/data/01_process-bulkrna/03_trimmomatic/* /mnt/scratch/5420/${ses}/output/03_trimmomatic/
+# cp -r /home/groups/EllrottLab/cell-dissociation/data/01_process-bulkrna/06_star/* /mnt/scratch/5420/${ses}/output/06_star/
+##########
+sudo /opt/acc/sbin/exadocker pull alexdobin/star:2.6.1d
+sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
+    alexdobin/star:2.6.1d \
+    STAR \
+    --runThreadN 11 \
+    --readFilesCommand zcat \
+    --genomeDir /tmp/output/06_star/genome_ref/ \
+    --readFilesIn /tmp/output/03_trimmomatic/${read1}_trimmed.fastq.gz /tmp/output/03_trimmomatic/${read2}_trimmed.fastq.gz \
+    --outFileNamePrefix /tmp/output/06_star/mapped/${basename}_mapped-unsorted.bam \
+    --outSAMtype BAM Unsorted
+
+###############
+# BAM QC
+###############
+# TBA
+
+##############################
+# Quantify reads: featureCounts
+##############################
+sudo /opt/acc/sbin/exadocker pull alexgilgal/featurecount:latest
+sudo /opt/acc/sbin/exadocker run --rm -v /mnt/scratch/5420/${ses}:/tmp \
+    alexgilgal/featurecount:latest \
+    featureCounts \
+    -T 10 -F GTF \
+    -g gene_id -t exon \
+    -R \
+    -s 1 \
+    -a /tmp/raw-data/metadata-STAR/gtf/TEMP-ref.gtf \
+    -o /tmp/output/07_ct_matrices/${basename}_CTmatrix.txt  \
+    /tmp/output/06_star/mapped/${basename}_mapped-unsorted.bam
+
+###############
+# Clean up workspace
+###############
 # copy files from scratch and clean up scratch
 cp -r /mnt/scratch/5420/${ses}/output/* /home/groups/EllrottLab/cell-dissociation/data/01_process-bulkrna/
 rm -rf /mnt/scratch/5420/${ses}
