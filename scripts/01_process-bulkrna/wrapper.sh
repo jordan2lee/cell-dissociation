fe8abedaeea14b128cc20d6e4f4d0de54188d86b
jordan2lee <jordanl.7890@gmail.com>
2020-03-06 16:09:13 -0800

    add normal patient sample for bulk prepro

--- scripts/01_process-bulkrna/wrapper.sh
+++ scripts/01_process-bulkrna/wrapper.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 ###########
 # PURPOSE: run preprocessing of bulk RNA
 ###########
@@ -14,3 +12,9 @@ sbatch RUN.sh /home/groups/EllrottLab/cell-dissociation patient_7320_B2 P2000996
 sbatch RUN.sh /home/groups/EllrottLab/cell-dissociation patient_7319_GT P2000996_02292020/FASTQ/SCC_7319_GT_R1.fastq.gz P2000996_02292020/FASTQ/SCC_7319_GT_R2.fastq.gz SCC_7319_GT 5
 # Patient  7320 - ground truth
 sbatch RUN.sh /home/groups/EllrottLab/cell-dissociation patient_7320_GT P2000996_02292020/FASTQ/SCC_7320_GT_R1.fastq.gz P2000996_02292020/FASTQ/SCC_7320_GT_R2.fastq.gz SCC_7320_GT 5
+
+
+# Patient normal
+sbatch RUN.sh /home/groups/EllrottLab/cell-dissociation patient_7319_N P2000996_02292020/FASTQ/SCC_7319_N_R1.fastq.gz P2000996_02292020/FASTQ/SCC_7319_N_R2.fastq.gz SCC_7319_N 5
+# Patient normal
+sbatch RUN.sh /home/groups/EllrottLab/cell-dissociation patient_7320_N P2000996_02292020/FASTQ/SCC_7320_N_R1.fastq.gz P2000996_02292020/FASTQ/SCC_7320_N_R2.fastq.gz SCC_7320_N 5
